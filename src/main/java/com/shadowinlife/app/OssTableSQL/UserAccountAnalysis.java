4780994bb8683774b73d03419305621b52bfe5fb
shadowinlife <shadowinlife@gmail.com>
2015-05-11 11:23:19 +0800

    Oozie job

--- src/main/java/com/shadowinlife/app/OssTableSQL/UserAccountAnalysis.java
+++ src/main/java/com/shadowinlife/app/OssTableSQL/UserAccountAnalysis.java
@@ -1,5 +1,10 @@
 package com.shadowinlife.app.OssTableSQL;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.apache.spark.sql.hive.HiveContext;
 
 import com.shadowinlife.app.LogAnalyse.ProcessTableSQL.CONSTANT;
@@ -42,23 +47,38 @@ public class UserAccountAnalysis {
             + "IF(index_iaccounttype IS NULL,-1,index_iaccounttype) as index_iaccounttype,"
             + "IF(index_iworldid is null,-1, index_iworldid) as index_iworldid,"
             + "count(DISTINCT suin) AS newcount, "
-            + "sum(useractivity(idayacti, %s)) AS useractivitynum,"
-            + "sum(userlost(idayacti, %s)) AS userlostnum,"
-            + "sum(usercomeback(idayacti,%s)) AS usercomebacknum,"
+            + "sum(useractivity(%s, %s)) AS useractivitynum,"
+            + "sum(userlost(%s, %s)) AS userlostnum,"
+            + "sum(usercomeback(%s,%s)) AS usercomebacknum,"
             + "sum(itimes) AS itimes,"
             + "sum(ionlinetime) AS ionlinetime "
-            + "FROM fat_login_user WHERE index_dtstatdate=DATE2LONG('%s') "
+            + "FROM fat_%s_user WHERE index_dtstatdate=DATE2LONG('%s') "
             + "group by index_igameid,index_iaccounttype,index_iworldid with cube) T1  "
             
             + "LEFT JOIN (SELECT IF(index_igameid IS NULL,-1,index_igameid) AS index_igameid,"
             + "IF(index_iaccounttype IS NULL,-1,index_iaccounttype) AS index_iaccounttype,"
             + "IF(index_iworldid is null,-1, index_iworldid) AS index_iworldid,"
             + "COUNT(DISTINCT suin) AS oldcount "
-            + "FROM fat_login_user WHERE index_dtstatdate=(DATE2LONG('%s')-1) "
+            + "FROM fat_%s_user WHERE index_dtstatdate=(DATE2LONG('%s')-%s) "
             + "GROUP BY index_igameid,index_iaccounttype,index_iworldid with cube) T2 "
             
             + "ON T1.index_igameid=T2.index_igameid AND T1.index_iaccounttype=T2.index_iaccounttype "
             + "AND T1.index_iworldid=T2.index_iworldid";
+    
+    private static String tbRegisterUser_TotalRegUser = "INSERT INTO TABLE oss_tbRegisterUser PARTITION(index_dtstatdate=%s) "
+            + "SELECT '%s'," //date 
+            + "%s,"  //period
+            + "IF(index_igameid IS NULL,-1,index_igameid) AS index_igameid,"
+            + "IF(index_iaccounttype IS NULL,-1,index_iaccounttype) as index_iaccounttype,"
+            + "IF(index_iworldid is null,-1, index_iworldid) as index_iworldid,"
+            + "count(DISTINCT suin) AS newcount, "
+            + "0,"
+            + "0,"
+            + "0,"
+            + "sum(itimes) AS itimes,"
+            + "sum(ionlinetime) AS ionlinetime "
+            + "FROM fat_login_user WHERE index_dtstatdate=DATE2LONG('%s') "
+            + "group by index_igameid,index_iaccounttype,index_iworldid with cube";
     
     /*
      * tbRegisterUserPeTypeDis
@@ -235,18 +255,65 @@ public class UserAccountAnalysis {
             + "where '%s' " //TODO
             + "group by igameid,iaccounttype,iworldid,datediff(dtstatdate, %s) grouping sets((igameid,iaccounttype,iworldid,datediff(dtstatdate, %s)))";
 
-    public static void create_tbRegisterUser(HiveContext sqlContext,int Mode,String date,int period){
+    public static void create_tbRegisterUser(HiveContext sqlContext,String strMode,String strDate){
         
         // Initialization hive UDF
         sqlContext.sql("use dbprocess");
         sqlContext.sql("ADD JAR hdfs://10-4-28-24:8020//udf.jar");
 
-        /*
-         * args: index_dtstatdate dtstatdate period for insert period for
-         * useractivity period for userlost period for usercomeback DATE DATE
-         */
-        String Create_tbRegisterUser_SQL = String.format(tbRegisterUser, CONSTANT.date2Long(date),
-                period, period, period, period, date, date);
-        sqlContext.sql(Create_tbRegisterUser_SQL);
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd"); 
+		Date date = new Date();
+		try {
+			date = sdf.parse(strDate);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Calendar time=Calendar.getInstance();
+		time.setTime(date);
+		
+		String strSql;
+		String strTableField = new String();
+		int iPeriod = 0;
+		int iType = time.get(Calendar.DAY_OF_WEEK);
+		if (1 == iType){//周的最后一天，需要进行周统计
+			iPeriod = 7;
+			strTableField = "iweekacti";
+			strSql = String.format(tbRegisterUser,CONSTANT.date2Long(strDate),
+					strDate,iPeriod,strTableField,1,strTableField,1,strTableField,1,
+					strMode,strDate,strMode,strDate,iPeriod);
+			sqlContext.sql(strSql);
+		}
+		
+		//是否需要进行月统计
+		time.add(Calendar.DATE, 1);
+		iType = time.get(Calendar.DAY_OF_MONTH);
+		if (1 == iType){//月的最后一天，需要进行月统计
+			iPeriod = 30;
+			strTableField = "imonthacti";
+			time.set(Calendar.MONDAY, time.get(Calendar.MONDAY) - 1);
+			time.add(Calendar.DATE, -1);
+			String strBeforeMonthDate = sdf.format(time.getTime());
+			
+			strSql = String.format(tbRegisterUser,CONSTANT.date2Long(strDate),
+					strDate,iPeriod,strTableField,1,strTableField,1,strTableField,1,
+					strMode,strDate,strMode,strBeforeMonthDate,0);
+			sqlContext.sql(strSql);
+		}
+        
+		//日统计，需要每天进行
+		iPeriod = 1;
+		strTableField = "idayacti";
+		strSql = String.format(tbRegisterUser,CONSTANT.date2Long(strDate),
+				strDate,iPeriod,strTableField,1,strTableField,1,strTableField,1,
+				strMode,strDate,strMode,strDate,iPeriod);
+		sqlContext.sql(strSql);
+        
+        //总注册用户，每天需要计算
+		iPeriod = -1;
+		strSql = String.format(tbRegisterUser_TotalRegUser,CONSTANT.date2Long(strDate),
+				strDate,iPeriod,strDate);
+		sqlContext.sql(strSql);
     }
 }
